2a2827fc3a0980078d1774058b024feddbd570b2
Muhammad Rafat Kurkar <84540534+KMKINGMAN@users.noreply.github.com>
2022-09-02 05:58:26 +0300

    add list command to show all account's backups list

---
+++ src/Commands/[A] Loader/list.ts
@@ -0,0 +1,19 @@
+import { CommandFilerType } from "../../bace-system/handler/cmd";
+const data = {
+    general: {
+        name: "list",
+        description: "to show account's list",
+        category: 'list',
+        examples: ["list"],
+        permissions: {
+            bot: "ADMINISTRATOR",
+            me: "ADMINISTRATOR"
+        },
+        usage: ["list"],
+        run: async(client , kmsg, args, manager)=> {
+            let data = client.accounts_dir.map(s=> `${s}\n`);
+            return manager.sendDone(kmsg.channel, `You Have those account Backup\n${data}`);
+        }
+    }
+} as CommandFilerType
+export default data;
