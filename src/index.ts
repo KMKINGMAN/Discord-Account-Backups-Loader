377b2ba4940e9f86c6934a4ea512757a1a278662
Muhammad Rafat Kurkar <84540534+KMKINGMAN@users.noreply.github.com>
2022-09-02 05:42:12 +0300

    add backup dir to the client obj

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import fs from "fs";
 import Client from "./bace-system/KMCodes";
+//ClientOPS , BackupFile Dir
 const client = new Client({
     intents: 98303, 
     partials: ["CHANNEL", "GUILD_MEMBER", "GUILD_SCHEDULED_EVENT", "MESSAGE", "REACTION", "USER"] 
-});
+}, "./backups");
 client.login(client.config.token);
